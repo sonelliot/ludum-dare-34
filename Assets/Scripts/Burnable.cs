652f17f74774f55c77907abbb900327e04ebe3f8
Eliexiyu <MatthewTJBurr@gmail.com>
2015-12-12 15:47:55 +0800

    Bruning now spreads

--- Assets/Scripts/Burnable.cs
+++ Assets/Scripts/Burnable.cs
@@ -8,15 +8,19 @@ public class Burnable : MonoBehaviour
         Unburnt, Burning, Burnt
     };
 
+    public float burnRadius = 5;
+
     [SerializeField]
     private State _state = State.Unburnt;
     private ParticleSystem _flames;
+    private SphereCollider _Trigger;
 
 	void Start()
     {
         _flames = transform.Find("Flames").gameObject
             .GetComponent<ParticleSystem>();
         _flames.Stop();
+        _Trigger = GetComponent<SphereCollider>();
 	}
 	
 	void Update()
@@ -24,10 +28,23 @@ public class Burnable : MonoBehaviour
         if (_state == State.Burning && _flames.isPlaying == false)
         {
             _flames.Play();
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, burnRadius);
+            int i = 0;
+            while (i < hitColliders.Length-1)
+            {
+                hitColliders[i].SendMessage("StartBurning");
+                i++;
+            }
         }
         else if (_state == State.Burnt && _flames.isPlaying == true)
         {
             _flames.Stop();
         }
 	}
+
+    IEnumerator StartBurning()
+    {
+        yield return new WaitForSeconds(3f);
+        _state = State.Burning;
+    }
 }
