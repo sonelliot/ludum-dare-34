dc8d7f5d4b05ee58463e197e9651d00c304e5dbb
Elliot Bulmer <sonelliot@gmail.com>
2015-12-14 17:00:47 +0800

    Try to fix sorting of particle systems

---
+++ Assets/Scripts/ParticleSort.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ParticleSort : MonoBehaviour
+{
+    public string layerName;
+
+    public void Awake()
+    {
+        var renderer = GetComponent<ParticleSystem>().GetComponent<Renderer>();
+        renderer.sortingLayerName = this.layerName;
+    }
+}
