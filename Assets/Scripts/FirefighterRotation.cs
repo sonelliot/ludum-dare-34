544851618298082058d7adb533e0425533daf8b9
Peter Danes <petergdanes@gmail.com>
2015-12-14 13:33:53 +0800

    Art Showcase

---
+++ Assets/Scripts/FirefighterRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirefighterRotation : MonoBehaviour {
+
+    public Sprite LSprite;
+    public Sprite LDSprite;
+    public Sprite DSprite;
+    public Sprite RDSprite;
+    public Sprite RSprite;
+    public Sprite RUSprite;
+    public Sprite USprite;
+    public Sprite LUSprite;
+
+    private SpriteRenderer _SR;
+
+	// Use this for initialization
+	void Start () {
+        _SR = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
